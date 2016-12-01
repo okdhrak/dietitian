bdf19d7242771f872887c6d872bf448b85fbf1d3
chanmako <nikukyulabo@gmail.com>
2016-12-01 20:59:23 +0900

    added webhook

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const LINE_CHANNEL_ACCESS_TOKEN = 'P/1kDx8RI8KTNEU6EAY2PmLh0rWVTUVqNCeIMs05rbHwU
 var express = require('express');
 var bodyParser = require('body-parser'); // 追加
 var request = require('request'); // 追加
+var mecab = require('mecabaas-client'); // 追加
 var app = express();
 
 // -----------------------------------------------------------------------------
@@ -39,7 +40,7 @@ app.get('/', function(req, res, next){
     }
 });*/
 
-app.post('/webhook', function(req, res, next){
+/*app.post('/webhook', function(req, res, next){
     res.status(200).end();
     for (var event of req.body.events){
         if (event.type == 'message' && event.message.text == 'ハロー'){
@@ -62,6 +63,23 @@ app.post('/webhook', function(req, res, next){
                 body: body,
                 json: true
             });
+        }
+    }
+});*/
+
+app.post('/webhook', function(req, res, next){
+    res.status(200).end();
+    for (var event of req.body.events){
+        if (event.type == 'message' && event.message.text){
+             // Mecabクラウドサービスでメッセージを解析
+            mecab.parse(event.message.text)
+            .then(
+                function(response){
+                      // 解析結果を出力
+                    console.log(response);
+                }
+            );
+
         }
     }
 });
