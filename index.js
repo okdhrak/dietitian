734186ba8fec976b39c81e6c2dfff7b41fec66d1
chanmako <nikukyulabo@gmail.com>
2016-12-01 20:43:51 +0900

    added webhook

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 // -----------------------------------------------------------------------------
+// 定数の設定
+const LINE_CHANNEL_ACCESS_TOKEN = 'あなたのChannl Access Token'; // 追加
+
+// -----------------------------------------------------------------------------
+
+// -----------------------------------------------------------------------------
 // モジュールのインポート
 var express = require('express');
 var bodyParser = require('body-parser'); // 追加
+var request = require('request'); // 追加
 var app = express();
 
 // -----------------------------------------------------------------------------
@@ -23,11 +30,38 @@ app.get('/', function(req, res, next){
 
 // webhook
 // LINEからのリクエストに対するレスポンス
-app.post('/webhook', function(req, res, next){
+/*app.post('/webhook', function(req, res, next){
     res.status(200).end();
     for (var event of req.body.events){
         if (event.type == 'message'){
             console.log(event.message);
+        }
+    }
+});*/
+
+app.post('/webhook', function(req, res, next){
+    res.status(200).end();
+    for (var event of req.body.events){
+        if (event.type == 'message' && event.message.text == 'ハロー'){
+            var headers = {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + LINE_CHANNEL_ACCESS_TOKEN
+            }
+            var body = {
+                replyToken: event.replyToken,
+                messages: [{
+                    type: 'text',
+                    text: 'こんにちはー'
+                }]
+            }
+            var url = 'https://api.line.me/v2/bot/message/reply';
+            request({
+                url: url,
+                method: 'POST',
+                headers: headers,
+                body: body,
+                json: true
+            });
         }
     }
 });
