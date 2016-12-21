4af3b9750be4441c503bd8f893e99a4b3924f820
chanmako <nikukyulabo@gmail.com>
2016-12-21 19:39:29 +0900

    added memory

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // -----------------------------------------------------------------------------
 // 定数の設定
-const LINE_CHANNEL_ACCESS_TOKEN = 'P/1kDx8RI8KTNEU6EAY2PmLh0rWVTUVqNCeIMs05rbHwUJr25umCi98oGhBBYpY+zpY4XLwpuBPZIiL8Hs7PJf8uKL5OwpH/3SK3Xr2a2lKBRlqhhneEa7YFWxP5eLVNWv38AsQR7IAzkuhydZuCJgdB04t89/1O/w1cDnyilFU='; // 追加
+//const LINE_CHANNEL_ACCESS_TOKEN = 'P/1kDx8RI8KTNEU6EAY2PmLh0rWVTUVqNCeIMs05rbHwUJr25umCi98oGhBBYpY+zpY4XLwpuBPZIiL8Hs7PJf8uKL5OwpH/3SK3Xr2a2lKBRlqhhneEa7YFWxP5eLVNWv38AsQR7IAzkuhydZuCJgdB04t89/1O/w1cDnyilFU='; // 追加
 
 // -----------------------------------------------------------------------------
 
@@ -11,6 +11,8 @@ var bodyParser = require('body-parser'); // 追加
 var request = require('request'); // 追加
 var mecab = require('mecabaas-client'); // 追加
 var shokuhin = require('shokuhin-db'); // 追加
+var memory = require('memory-cache'); // 追加
+var dietitian = require('./dietitian'); // 追加
 var app = express();
 
 // -----------------------------------------------------------------------------
@@ -85,7 +87,7 @@ app.get('/', function(req, res, next){
     }
 });*/
 
-app.post('/webhook', function(req, res, next){
+/*app.post('/webhook', function(req, res, next){
     res.status(200).end();
     for (var event of req.body.events){
         if (event.type == 'message' && event.message.text){
@@ -109,6 +111,77 @@ app.post('/webhook', function(req, res, next){
             ).then(
                 function(response){
                     console.log(response);
+                }
+            );
+        }
+    }
+});*/
+
+app.post('/webhook', function(req, res, next){
+    res.status(200).end();
+    for (var event of req.body.events){
+        if (event.type == 'message' && event.message.text){
+            mecab.parse(event.message.text)
+            .then(
+                function(response){
+                    var foodList = [];
+                    for (var elem of response){
+                        if (elem.length > 2 && elem[1] == '名詞'){
+                            foodList.push(elem);
+                        }
+                    }
+                    var gotAllNutrition = [];
+                    if (foodList.length > 0){
+                        for (var food of foodList){
+                            gotAllNutrition.push(shokuhin.getNutrition(food[0]));
+                        }
+                        return Promise.all(gotAllNutrition);
+                    }
+                }
+            ).then(
+                function(responseList){
+                    // 記憶すべき情報を整理する。
+                    var botMemory = {
+                        confirmedFoodList: [],
+                        toConfirmFoodList: [],
+                        confirmingFood: null
+                    }
+                    for (var nutritionList of responseList){
+                        if (nutritionList.length == 0){
+                            // 少なくとも今回の食品DBでは食品と判断されなかったのでスキップ。
+                            continue;
+                        } else if (nutritionList.length == 1){
+                            // 該当する食品が一つだけ見つかったのでこれで確定した食品リストに入れる。
+                            botMemory.confirmedFoodList.push(nutritionList[0]);
+                        } else if (nutritionList.length > 1){
+                            // 複数の該当食品が見つかったのでユーザーに確認するリストに入れる。
+                            botMemory.toConfirmFoodList.push(nutritionList);
+                        }
+                    }
+
+                    /*
+                     * もし確認事項がなければ、合計カロリーを返信して終了。
+                     * もし確認すべき食品があれば、質問して現在までの状態を記憶に保存。
+                     */
+                    if (botMemory.toConfirmFoodList.length == 0 && botMemory.confirmedFoodList.length > 0){
+                        console.log('Going to reply the total calorie.');
+
+                        // 確認事項はないので、確定した食品のカロリーの合計を返信して終了。
+                        dietitian.replyTotalCalorie(event.replyToken, botMemory.confirmedFoodList);
+
+                    } else if (botMemory.toConfirmFoodList.length > 0){
+                        console.log('Going to ask which food the user had');
+
+                        // どの食品が正しいか確認する。
+                        dietitian.askWhichFood(event.replyToken, botMemory.toConfirmFoodList[0]);
+
+                        // ユーザーに確認している食品は確認中のリストに入れ、確認すべきリストからは削除。
+                        botMemory.confirmingFood = botMemory.toConfirmFoodList[0];
+                        botMemory.toConfirmFoodList.splice(0, 1);
+
+                        // Botの記憶に保存
+                        memory.put(event.source.userId, botMemory);
+                    }
                 }
             );
         }
